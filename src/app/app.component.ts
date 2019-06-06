7fedf8562a71e79a7541d6c736bf3a6143a03c9a
chuma <chumadamane40@gmail.com>
2019-06-06 14:07:35 +0200

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ADDRGETNETWORKPARAMS } from 'dns';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'exam';
+
+  foods = [ "bread", "milk", "meat", "eggs", "rice"]
+
+
+  remove(food){
+    let index= this.foods.indexOf(food)
+    this.foods.splice(index,1)
+
+}
 }
